0a9aff8ffcf94493dae3d05ce8972d2c43898f07
Anh Khang <taicuoi664@gmail.com>
2024-08-07 18:35:32 +0700

    Add DeQui_Bai10_TimKiemNhiPhan

--- Code/Source.cpp
+++ Code/Source.cpp
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <conio.h>
 #include <math.h>
 
@@ -29,6 +29,28 @@ int tinhBieuThuc(int n)
 	else return tinhBieuThuc(n-5) + tinhBieuThuc(n-4) + tinhBieuThuc(n-3) + tinhBieuThuc(n-2) + tinhBieuThuc(n-1);
 }
 
+int timKiemNhiPhan(int arr[], int left, int right, int x) {
+    if (right >= left) {
+        int mid = left + (right - left) / 2;
+        
+        // Nếu phần tử nằm ở giữa
+        if (arr[mid] == x) {
+            return mid;
+        }
+
+        // Nếu phần tử nhỏ hơn giá trị ở giữa, tìm ở nửa trái
+        if (arr[mid] > x) {
+            return timKiemNhiPhan(arr, left, mid - 1, x);
+        }
+
+        // Nếu phần tử lớn hơn giá trị ở giữa, tìm ở nửa phải
+        return timKiemNhiPhan(arr, mid + 1, right, x);
+    }
+    
+    // Nếu phần tử không có trong mảng
+    return -1;
+}
+
 int main()
 {
 	int m, n;
@@ -46,6 +68,22 @@ int main()
 	printf("Ket qua cua cap so cong: %d", capSoCong(n,a,r));
 	printf("\n---------------------------------\n");
 	printf("Ket qua cua phep tinh bieu thuc S(n) la: %d", tinhBieuThuc(n1));
+	printf("\n---------------------------------\n");
+	int arr[] = {2, 3, 4, 10, 40};
+	for (int i = 0; i < 5; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+	printf("\n");
+	int x;
+	printf("Nhap x can tim:");scanf("%d", &x);
+	int result = timKiemNhiPhan(arr, 0, 5-1, x);
+    
+    if (result != -1) {
+        printf("Phan tu duoc tim thay o chi so %d\n", result);
+    } else {
+        printf("Phan tu khong co trong mang\n");
+    }
 	getch();
 	return 0;
 }
