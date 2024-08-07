30a301fa3f0810e16dcbb31599a20f634cfd772b
Anh Khang <taicuoi664@gmail.com>
2024-08-07 18:22:25 +0700

    Add DeQui_Bai06_TinhBieuThucS(n)

--- Code/Source.cpp
+++ Code/Source.cpp
@@ -20,6 +20,15 @@ int capSoCong(int n, int a, int r)
 	else return r + capSoCong(n-1,a,r);
 }
 
+int tinhBieuThuc(int n)
+{
+	if (n<6)
+	{
+		return n;
+	}
+	else return tinhBieuThuc(n-5) + tinhBieuThuc(n-4) + tinhBieuThuc(n-3) + tinhBieuThuc(n-2) + tinhBieuThuc(n-1);
+}
+
 int main()
 {
 	int m, n;
@@ -29,10 +38,14 @@ int main()
 	scanf("%d", &n);
 	printf("Ket qua phep toan: %d", tinhMu(m,n));
 	printf("\n---------------------------------\n");
-	int a,r;
+	int a,r,n1;
+	printf("Nhap gia tri phan tu n:");
+	scanf("%d", &n1);
 	printf("Nhap hang dau a:"); scanf("%d", &a);
 	printf("Nhap cong sai r:"); scanf("%d", &r);
 	printf("Ket qua cua cap so cong: %d", capSoCong(n,a,r));
+	printf("\n---------------------------------\n");
+	printf("Ket qua cua phep tinh bieu thuc S(n) la: %d", tinhBieuThuc(n1));
 	getch();
 	return 0;
 }
