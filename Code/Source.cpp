3ec86290e85543e15f6006a5c55429cb6f96a732
Anh Khang <taicuoi664@gmail.com>
2024-08-07 18:14:11 +0700

    Add DeQui_Bai03_TinhCapSoCong

--- Code/Source.cpp
+++ Code/Source.cpp
@@ -11,6 +11,15 @@ int tinhMu(int m, int n)
 	else return m*tinhMu(m,n-1);
 }
 
+int capSoCong(int n, int a, int r)
+{
+	if (n==1)
+	{
+		return a;
+	}
+	else return r + capSoCong(n-1,a,r);
+}
+
 int main()
 {
 	int m, n;
@@ -19,6 +28,11 @@ int main()
 	printf("Nhap n:");
 	scanf("%d", &n);
 	printf("Ket qua phep toan: %d", tinhMu(m,n));
+	printf("\n---------------------------------\n");
+	int a,r;
+	printf("Nhap hang dau a:"); scanf("%d", &a);
+	printf("Nhap cong sai r:"); scanf("%d", &r);
+	printf("Ket qua cua cap so cong: %d", capSoCong(n,a,r));
 	getch();
 	return 0;
 }
